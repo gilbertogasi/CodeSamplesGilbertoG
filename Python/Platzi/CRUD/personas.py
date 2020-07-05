a43836086a9718ddb236bea7021912f0bf23677d
gilbertog <gilbertoaguerrero@gmail.com>
2020-07-05 13:00:13 -0500

    Se actualiza repositorio de practicas en Python, con curso de creación de un CRUD.

---
+++ Python/Platzi/CRUD/personas.py
@@ -0,0 +1,20 @@
+
+
+
+class Person:
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def say_hello(self):
+        print('Hello, my name is {} and I am {} years old'.format(self.name,self.age))
+
+
+if __name__ == '__main__':
+    person = Person('Gilberto', 36)
+
+    print('Age {}'.format(person.age))
+    person.say_hello()
+
+
